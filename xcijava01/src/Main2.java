35569e7d7d7611119295ab763d27d79a6c2b93fa
jun <jun@192.168.100.98>
2023-10-19 08:46:16 +0900

    현대IT&E 교육 3일차

--- xcijava01/src/Main2.java
+++ xcijava01/src/Main2.java
@@ -5,30 +5,69 @@ import java.util.StringTokenizer;
 
 public class Main2 {
 	
+	static int max,min;
+	static int[] Operator;
+	static int[] A;
+	static int N;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] A = new int[N];
+		A = new int[N];
 		for(int i = 0; i < N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
 		st = new StringTokenizer(br.readLine());
 		
-		int[] Operater = new int[4]; // plus, minus, mul, div
+		Operator = new int[4]; // plus, minus, mul, div
 		int sum = 0;
 		for(int i = 0; i < 4; i++) {
-			Operater[i] = Integer.parseInt(st.nextToken());
-			sum += Operater[i];
+			Operator[i] = Integer.parseInt(st.nextToken());
+			sum += Operator[i];
 		}
 		
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
+		min = Integer.MAX_VALUE;
+		max = Integer.MIN_VALUE;
 		
-		
+		dfs(A[0], 1);
+		System.out.println(max + "\n" + min);
 		
 	}
+	
+	   public static void dfs(int num, int idx) {
+	        if (idx == N) {
+	            max = Math.max(max, num);
+	            min = Math.min(min, num);
+	            return;
+	        }
+
+	        for (int i = 0; i < 4; i++) {
+	            // 연산자 개수가 1개 이상인 경우
+	            if (Operator[i] > 0) {
+	                Operator[i]--;
+
+	                switch (i) {
+	                    case 0:
+	                        dfs(num + A[idx], idx + 1);
+	                        break;
+	                    case 1:
+	                        dfs(num - A[idx], idx + 1);
+	                        break;
+	                    case 2:
+	                        dfs(num * A[idx], idx + 1);
+	                        break;
+	                    case 3:
+	                        dfs(num / A[idx], idx + 1);
+	                        break;
+	                }
+
+	                // 재귀 호출이 종료되면 다시 해당 연산자 개수를 복구
+	                Operator[i]++;
+	            }
+	        }
+	   }
+	
 }
