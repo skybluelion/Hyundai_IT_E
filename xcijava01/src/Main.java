349f224a3b845e68eae2bdbbeeb120b339d5baf3
skybluelion <brotherjun96@gmail.com>
2023-10-23 10:53:58 +0900

    현대 IT&E 교육 6일

--- xcijava01/src/Main.java
+++ xcijava01/src/Main.java
@@ -1,15 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Stack<Integer> stack = new Stack<Integer>();
 		int N = Integer.parseInt(br.readLine());
-		System.out.println(N * 24);
+		int[] seq = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
+		}
 
+		for(int i = 0; i < N; i++) {
+			while(!stack.isEmpty() && seq[stack.peek()] < seq[i]) {
+				seq[stack.pop()] = seq[i];
+			}
+			stack.push(i);
+		}
+		
+		// 스택의 모든 원소를 pop 하면서 해당 인덱스 밸류 -1 초기화
+		while(!stack.isEmpty()) {
+			seq[stack.pop()] = -1;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			sb.append(seq[i]).append(' ');
+		}
+		System.out.println(sb);
+		
+		br.close();
 	}
 
 }
